7c56aa9d6306950aa50a4ebe637b54bc944d1f23
AdrianoAnimus <adrianoanimus@gmail.com>
2021-11-11 10:07:19 -0300

    Import EditorState  - from draft - js

--- react-editor/src/components/TextEditor.js
+++ react-editor/src/components/TextEditor.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
 import { Editor } from "react-draft-wysiwyg"
+import { EditorState } from 'draft-js'
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
 
 export default class TextEditor extends Component {
+
+    state = {
+        editorState:EditorState.createEmpty(),
+    }
+
+    onEditorStateChange = (editorState) => {
+        this.setState({
+            editorState, 
+        })
+    }
+
+
     render() {
+        const {editorState} = this.state
         return (
             <div>
                 <Editor
