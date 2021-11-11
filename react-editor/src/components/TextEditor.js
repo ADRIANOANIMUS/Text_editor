e4e2a0218b84ccc9197f5757c19326591cadf4a4
AdrianoAnimus <adrianoanimus@gmail.com>
2021-11-11 09:53:20 -0300

    installing package react-draft-wysiwyg

--- react-editor/src/components/TextEditor.js
+++ react-editor/src/components/TextEditor.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { Editor } from "react-draft-wysiwyg"
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
 
 export default class TextEditor extends Component {
     render() {
         return (
             <div>
-                <h1>Text Editor</h1>
+                <Editor
+  editorState={editorState}
+  toolbarClassName="toolbarClassName"
+  wrapperClassName="wrapperClassName"
+  editorClassName="editorClassName"
+  onEditorStateChange={this.onEditorStateChange}
+/>
             </div>
         )
     }
